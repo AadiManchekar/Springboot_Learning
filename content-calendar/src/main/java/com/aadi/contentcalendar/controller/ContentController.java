ece9e3f7611c360cf72c8da2f080ef7225d7d1ee
AadiManchekar <aadimanchekar2002@gmail.com>
2023-07-22 12:40:18 +0530

    modified controller

--- content-calendar/src/main/java/com/aadi/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/com/aadi/contentcalendar/controller/ContentController.java
@@ -3,8 +3,15 @@ package com.aadi.contentcalendar.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aadi.contentcalendar.repository.ContentCollectionRepository;
+
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
 }
