bb7caae9f721af38b74124a51d1caa21c183ec9e
AadiManchekar <aadimanchekar2002@gmail.com>
2023-07-27 10:57:37 +0530

    createQuiz

--- quizapp/src/main/java/com/aadi/quizapp/model/Question.java
+++ quizapp/src/main/java/com/aadi/quizapp/model/Question.java
@@ -1,13 +1,17 @@
 package com.aadi.quizapp.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
+@NoArgsConstructor // Add this annotation to generate a default constructor
 @Entity
 public class Question {
 
@@ -15,12 +19,36 @@ public class Question {
     @SequenceGenerator(name = "question_id_sequence", sequenceName = "question_id_sequence", allocationSize = 1, initialValue = 21)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "question_id_sequence")
     private Integer id;
+
+    @NonNull
+    @Column(nullable = false)
     private String questionTitle;
+
+    @NonNull
+    @Column(nullable = false)
     private String option1;
+
+    @NonNull
+    @Column(nullable = false)
     private String option2;
+
+    @NonNull
+    @Column(nullable = false)
     private String option3;
+
+    @NonNull
+    @Column(nullable = false)
     private String option4;
+
+    @NonNull
+    @Column(nullable = false)
     private String rightAnswer;
+
+    @NonNull
+    @Column(nullable = false)
     private String difficultyLevel;
+
+    @NonNull
+    @Column(nullable = false)
     private String category;
 }
--- quizapp/src/main/java/com/aadi/quizapp/service/QuestionService.java
+++ quizapp/src/main/java/com/aadi/quizapp/service/QuestionService.java
@@ -14,50 +14,31 @@ import com.aadi.quizapp.model.Question;
 @Service
 public class QuestionService {
 
-    private Integer generateQuestionId() {
-        // Logic to generate a unique ID for the question
-        // You can use a random generator, timestamp, or any other mechanism to generate
-        // the ID
-        // For this example, let's generate a random integer between 1 and 1000
-        return (int) (Math.random() * 1000) + 1;
-    }
-
     @Autowired
     QuestionDao questionDao;
 
     public ResponseEntity<List<Question>> getAllQuestions() {
         try {
-            return new ResponseEntity<List<Question>>(questionDao.findAll(), HttpStatus.OK);
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return new ResponseEntity<List<Question>>(new ArrayList<Question>(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ArrayList<Question>(), HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
         try {
-            return new ResponseEntity<List<Question>>(questionDao.findByCategory(category), HttpStatus.OK);
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return new ResponseEntity<List<Question>>(new ArrayList<Question>(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ArrayList<Question>(), HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<String> addQuestion(Question question) {
-        // if (question.getId() == null) {
-        // // Generate an ID for the question
-        // question.setId(generateQuestionId());
-        // }
-
-        // questionDao.save(question);
-        // return new ResponseEntity<String>("Success", HttpStatus.CREATED);
         try {
-            if (question.getId() == null) {
-                // Generate an ID for the question
-                question.setId(generateQuestionId());
-            }
 
             questionDao.save(question);
             return new ResponseEntity<>("Success", HttpStatus.CREATED);
