30e58fabdef16fec2a322bfa05a1e8339d3137ee
AadiManchekar <aadimanchekar2002@gmail.com>
2023-08-01 00:13:40 +0530

    fixed bug

--- api/src/main/java/com/pokemonreview/api/controller/PokemonController.java
+++ api/src/main/java/com/pokemonreview/api/controller/PokemonController.java
@@ -3,8 +3,12 @@ package com.pokemonreview.api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,16 @@ public class PokemonController {
         pokemons.add(new Pokemon(2, "Pikachu", "Electric"));
         pokemons.add(new Pokemon(3, "Alakazam", "Phsycic"));
         return ResponseEntity.ok(pokemons);
+    }
+
+    @GetMapping("pokemon/{id}")
+    public ResponseEntity<Pokemon> getPokemonById(@PathVariable Integer id) {
+        return ResponseEntity.ok(new Pokemon(id, "Charizad", "Fire"));
+    }
+
+    @PostMapping("pokemon/create")
+    public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon) {
+        System.out.println(pokemon);
+        return new ResponseEntity<>(pokemon, HttpStatus.CREATED);
     }
 }
