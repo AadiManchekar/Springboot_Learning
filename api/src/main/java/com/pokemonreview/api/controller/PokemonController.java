50383d699c94aa681443f674a605f475588cc718
AadiManchekar <aadimanchekar2002@gmail.com>
2023-08-01 01:48:12 +0530

    POST PUT DELETE

--- api/src/main/java/com/pokemonreview/api/controller/PokemonController.java
+++ api/src/main/java/com/pokemonreview/api/controller/PokemonController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +34,21 @@ public class PokemonController {
         return ResponseEntity.ok(new Pokemon(id, "Charizad", "Fire"));
     }
 
-    @PostMapping("pokemon/create")
+    @PostMapping("pokemon")
     public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon) {
         System.out.println(pokemon);
         return new ResponseEntity<>(pokemon, HttpStatus.CREATED);
+    }
+
+    @PutMapping("pokemon/{id}")
+    public ResponseEntity<Pokemon> updatePokemon(@RequestBody Pokemon pokemon, @PathVariable("id") Integer pokemonId) {
+        System.out.println(pokemon);
+        return ResponseEntity.ok(pokemon);
+    }
+
+    @DeleteMapping("pokemon/{id}")
+    public ResponseEntity<String> deletePokemon(@PathVariable("id") Integer pokemonId) {
+        System.out.println(pokemonId);
+        return ResponseEntity.ok("success");
     }
 }
