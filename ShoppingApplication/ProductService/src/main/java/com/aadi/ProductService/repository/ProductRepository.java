bfd390d687ebc036eb5c8856d1fff2489febce34
AadiManchekar <aadimanchekar2002@gmail.com>
2023-08-10 18:24:00 +0530

    Starter

---
+++ ShoppingApplication/ProductService/src/main/java/com/aadi/ProductService/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.aadi.ProductService.repository;
+
+import com.aadi.ProductService.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// no need for @Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {}
---
+++ ShoppingApplication/ProductService/src/main/java/com/aadi/ProductService/service/Implementations/ProductServiceImplV1.java
@@ -0,0 +1,10 @@
+package com.aadi.ProductService.service.Implementations;
+
+import org.springframework.stereotype.Service;
+
+import com.aadi.ProductService.service.ProductService;
+
+@Service
+public class ProductServiceImplV1 implements ProductService {
+
+}
