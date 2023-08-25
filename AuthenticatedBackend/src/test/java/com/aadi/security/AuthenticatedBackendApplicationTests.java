3d4b467b48af055a19a4f90cd0e7e1e29efd9525
AadiManchekar <aadimanchekar2002@gmail.com>
2023-08-25 12:37:10 +0530

    starter

---
+++ AuthenticatedBackend/src/test/java/com/aadi/security/AuthenticatedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
